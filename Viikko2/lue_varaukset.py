0e3c2171fbca39467961e89265b2c03f06c413de
Emmi Räisälä <emmi.raisala@centria.fi>
2025-11-09 12:29:50 +0200

    Print data of the file prettily

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -25,13 +25,39 @@ def main():
         varaus = f.read().strip()
 
     # Tulostetaan varaus konsoliin
-    print(varaus)
+    #print(varaus)
+
+    from datetime import datetime
 
     # Kokeile näitä
     #print(varaus.split('|'))
-    #varausId = varaus.split('|')[0]
+    varausId = int(varaus.split('|')[0])
+    varaaja = varaus.split('|')[1]
+    paiva = datetime.strptime(varaus.split('|')[2], "%Y-%m-%d").date()
+    suomalainenPaiva = paiva.strftime("%d.%m.%Y")
+    aika = datetime.strptime(varaus.split('|')[3], "%H:%M").time()
+    suomalainenAika = aika.strftime("%H.%M")
+    tunnit = int(varaus.split('|')[4])
+    tuntihinta = float(varaus.split('|')[5])
+    kokonaishinta = tunnit * tuntihinta
+    maksettu = bool(varaus.split('|')[6])
+    kohde = varaus.split('|')[7]
+    puhelin = varaus.split('|')[8]
+    email = varaus.split('|')[9]
     #print(varausId)
     #print(type(varausId))
+    print(f"Varausnumero: {varausId}")
+    print(f"Varaaja: {varaaja}")
+    print(f"Päivämäärä: {suomalainenPaiva}")
+    print(f"Aloitusaika: {suomalainenAika}")
+    print(f"Tuntimäärä: {tunnit}")
+    print(f"Tuntihinta: {tuntihinta} €")
+    print(f"Kokonaishinta: {kokonaishinta} €")
+    print(f"Maksettu: {'Kyllä' if maksettu else 'Ei'}")
+    print(f"Kohde: {kohde}")
+    print(f"Puhelin: {puhelin}")
+    print(f"Sähköposti: {email}")
+
     """
     Edellisen olisi pitänyt tulostaa numeron 123, joka
     on oletuksena tekstiä.
