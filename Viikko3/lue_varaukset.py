9d3b843aebaf69c846ac4b3c4b2cb6a6a55ebda0
Emmi Räisälä <emmi.raisala@centria.fi>
2025-11-17 11:34:18 +0200

    Did week 3

--- Viikko3/lue_varaukset.py
+++ Viikko3/lue_varaukset.py
@@ -18,34 +18,71 @@ Sähköposti: anna.virtanen@example.com
 """
 from datetime import datetime
 
+def hae_varausnumero(varaus):
+    varausnumero = int(varaus[0])
+    print(f"Varausnumero: {varausnumero}")
+
 def hae_varaaja(varaus):
     nimi = varaus[1]
     print(f"Varaaja: {nimi}")
 
-def main():
-    # Maaritellaan tiedoston nimi suoraan koodissa
-    varaukset = "varaukset.txt"
+def hae_paiva(varaus):
+    paivamaara = datetime.strptime(varaus[2], "%Y-%m-%d").date()
+    suomalainenPaiva = paivamaara.strftime("%d.%m.%Y")
+    print(f"Päivämäärä: {suomalainenPaiva}")
 
-    # Avataan tiedosto, luetaan ja splitataan sisalto
+def hae_aloitusaika(varaus):
+    aloitusaika = datetime.strptime(varaus [3], "%H:%M").time()
+    suomalainenaika = aloitusaika.strftime("%H:%M")
+    print (f"Aloitusaika: {suomalainenaika}")
+
+def hae_tuntimaara(varaus):
+    tuntimaara = int(varaus [4])
+    print (f"Tuntimäärä: {tuntimaara} ")
+
+def hae_tuntihinta(varaus):
+    tuntihinta = float(varaus[5])
+    print (f"Tuntihinta:", f"{tuntihinta:.2f}".replace('.', ','), "€")
+
+def laske_kokonaishinta(varaus):
+    kokonaishinta = int(varaus [4]) * float(varaus[5])
+    print (f"Kokonaishinta:", f"{kokonaishinta:.2f}".replace('.', ','), "€")
+    
+def hae_maksettu(varaus):
+    maksettu = bool(varaus[6])
+    print (f"Maksettu: ", f"{'Kyllä' if maksettu else 'Ei'}")
+    
+def hae_kohde(varaus):
+    kohde = (varaus[7])
+    print (f"Kohde: {kohde} ")
+    
+def hae_puhelin(varaus):
+    puhelin = varaus [8]
+    print (f"Puhelin: {puhelin}")
+
+def hae_sahkoposti(varaus):
+    sahkoposti = varaus [9]
+    print (f"Sähköposti: {sahkoposti}")
+
+def main():
+    varaukset = "varaukset.txt"
+    
     with open(varaukset, "r", encoding="utf-8") as f:
         varaus = f.read().strip()
         varaus = varaus.split('|')
 
-    # Toteuta loput funktio hae_varaaja(varaus) mukaisesti
-    # Luotavat funktiota tekevat tietotyyppien muunnoksen
-    # ja tulostavat esimerkkitulosteen mukaisesti
-
-    #hae_varausnumero(varaus)
+    hae_varausnumero(varaus)
     hae_varaaja(varaus)
-    #hae_paiva(varaus)
-    #hae_aloitusaika(varaus)
-    #hae_tuntimaara(varaus)
-    #hae_tuntihinta(varaus)
-    #laske_kokonaishinta(varaus)
-    #hae_maksettu(varaus)
-    #hae_kohde(varaus)
-    #hae_puhelin(varaus)
-    #hae_sahkoposti(varaus)
+    hae_paiva(varaus)
+    hae_aloitusaika(varaus)
+    hae_tuntimaara(varaus)
+    hae_tuntihinta(varaus)
+    laske_kokonaishinta(varaus)
+    hae_maksettu(varaus)
+    hae_kohde(varaus)
+    hae_puhelin(varaus)
+    hae_sahkoposti(varaus)
+
 
 if __name__ == "__main__":
     main()
